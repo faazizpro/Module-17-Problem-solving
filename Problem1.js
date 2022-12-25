b0e8e4d01a21af4db48d99ad7e5ff79dee42ca99
faazizpro <fahadafs11@gmail.com>
2022-12-25 11:17:15 +0600

    Problem 1 has been solved

---
+++ Problem1.js
@@ -0,0 +1,103 @@
+/*
+Problem 1:
+You and your friends Tom, Jane, Peter and John got their final exam results. Your total
+score is 85, Tom’s total score is 66, Jane’s total score is 95, Peter’s total score is 56
+and John’s total score is 40. The grading chart is
+80 or above A grade
+60 or above B grade
+50 or above C grade
+40 or above D grade
+39 or less => F grade
+
+Write a program to find your and your friends’ grades using if-else.
+*/
+
+let fahadScore = 85;
+let tomScore = 66;
+let janeScore = 95;
+let peterScore = 56;
+let johnScore = 40;
+
+if(fahadScore >= 80){
+    console.log('Fahad got A grade');
+}
+else if(fahadScore >= 60){
+    console.log('Fahad got B grade');
+}
+else if(fahadScore >= 50){
+    console.log('Fahad got C grade');
+}
+else if(fahadScore >= 40){
+    console.log('Fahad got D grade');
+}
+else if(fahadScore <= 39){
+    console.log('Fahad got F grade');
+}
+
+
+if(tomScore >= 80){
+    console.log('Tom got A grade');
+}
+else if(tomScore >= 60){
+    console.log('Tom got B grade');
+}
+else if(tomScore >= 50){
+    console.log('Tom got C grade');
+}
+else if(tomScore >= 40){
+    console.log('Tom got D grade');
+}
+else if(tomScore <= 39){
+    console.log('Tom got F grade');
+}
+
+
+if(janeScore >= 80){
+    console.log('Jane got A grade');
+}
+else if(janeScore >= 60){
+    console.log('Jane got B grade');
+}
+else if(janeScore >= 50){
+    console.log('Jane got C grade');
+}
+else if(janeScore >= 40){
+    console.log('Jane got D grade');
+}
+else if(janeScore <= 39){
+    console.log('Jane got F grade');
+}
+
+
+if(peterScore >= 80){
+    console.log('Peter got A grade');
+}
+else if(peterScore >= 60){
+    console.log('Peter got B grade');
+}
+else if(peterScore >= 50){
+    console.log('Peter got C grade');
+}
+else if(peterScore >= 40){
+    console.log('Peter got D grade');
+}
+else if(peterScore <= 39){
+    console.log('Peter got F grade');
+}
+
+
+if(johnScore >= 80){
+    console.log('John got A grade');
+}
+else if(johnScore >= 60){
+    console.log('John got B grade');
+}
+else if(johnScore >= 50){
+    console.log('John got C grade');
+}
+else if(johnScore >= 40){
+    console.log('John got D grade');
+}
+else if(johnScore <= 39){
+    console.log('John got F grade');
+}
